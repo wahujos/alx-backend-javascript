bf6736cd09aa9dcccbf358ab4e00eea333f8ca6a
wahujos <jwahu30@gmail.com>
2024-04-25 05:30:06 +0300

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,19 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+getFullResponseFromAPI(true)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
