281b7967566fc2f1125e27b45c6f92107adc1c51
wahujos <jwahu30@gmail.com>
2024-04-25 22:17:25 +0300

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,14 @@
 export default class Building {
   constructor(sqft) {
+    // this._sqft = sqft;
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   // getter
   get sqft() {
     return this._sqft;
-  }
-
-  // abstract method
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
