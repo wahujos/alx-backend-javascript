efb07ba90e379a5f60895436303230d989c03f74
wahujos <jwahu30@gmail.com>
2024-04-26 23:13:03 +0300

    12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      const departments = Object.keys(employeesList);
+      return departments.length;
+    },
+  };
+}
