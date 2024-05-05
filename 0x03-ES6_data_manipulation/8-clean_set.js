254c01bd61fd2495ccfa6c677e1934701c6999ad
wahujos <jwahu30@gmail.com>
2024-05-05 19:56:52 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,30 @@
+// export default function cleanSet(set, startString) {
+//   const filteredValues = [];
+//   if (startString === '' || typeof (startString) !== 'string') {
+//     return '';
+//   }
+//   set.forEach((element) => {
+//     if (element.startsWith(startString)) {
+//       filteredValues.push(element.substring(startString.length));
+//     }
+//   });
+//   return filteredValues.join('-');
+// }
+
+/**
+ * Returns a string of set values that start with a specific string.
+ * @param {Set} set - The set of values.
+ * @param {string} startString - The string to check if values start with.
+ * @returns {string} - A string containing all values of the set that start with startString
+ */
 export default function cleanSet(set, startString) {
-  const filteredValues = [];
-  if (startString === '' || typeof (startString) !== 'string') {
+  // Check if startString is undefined or empty
+  if (startString === undefined || startString.length === 0) {
     return '';
   }
-  set.forEach((element) => {
-    if (element.startsWith(startString)) {
-      filteredValues.push(element.substring(startString.length));
-    }
-  });
-  return filteredValues.join('-');
+
+  return [...set]
+    .filter((value) => value.startsWith(startString)) // Filter values that start with startString
+    .map((value) => value.slice(startString.length)) // Map to remove startString from each value
+    .join('-'); // Join the filtered and mapped values with '-'
 }
