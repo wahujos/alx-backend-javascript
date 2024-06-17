d4b032f5afa5be20dde26edcfe43887888167e08
wahujos <jwahu30@gmail.com>
2024-06-17 23:17:23 +0300

    qn 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+// create a function named displayMessage that prints
+// in STDOUT the string argument.
+
+function displayMessage(string) {
+    console.log(string);
+}
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,7 @@
+## 0x05. NodeJS Basics
+
+## Back-end
+## JavaScript
+## ES6
+## NodeJS
+## ExpressJS
