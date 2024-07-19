be4fa0c224ae1b6814b8dbc4d8255bdfcc79ebfc
wahujos <jwahu30@gmail.com>
2024-07-20 01:24:03 +0300

    move to cohort 18

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,8 +0,0 @@
-// create a function named displayMessage that prints
-// in STDOUT the string argument.
-
-function displayMessage(string) {
-  console.log(string);
-}
-
-module.exports = displayMessage;
--- 0x05-Node_JS_basic/README.md
+++
@@ -1,7 +0,0 @@
-## 0x05. NodeJS Basics
-
-## Back-end
-## JavaScript
-## ES6
-## NodeJS
-## ExpressJS
