2f97d09287c2b7c8c7033e9473b9d1ac9cfd0759
wahujos <jwahu30@gmail.com>
2024-07-24 00:31:06 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,6 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n', () => {
+      process.exit();
+    });
+  } else {
+    process.exit();
+  }
 });
