9523f6c5455a4bac354dd90993c21468d80490b4
wahujos <jwahu30@gmail.com>
2024-07-26 02:30:50 +0300

    add test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,15 +5,20 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function() {
   it('should add whole positive numbers correctly', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3), 4);
   });
 
   it('should round and add positive whole and decimal number correctly', function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
 
   it('should round and add decimal numbers correctly', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   });
+  it('should round and add 1.5 and 3.7', function() {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  })
+  it('should and and round a zero and a decimal number', function() {
+    assert.equal(calculateNumber(0, 2.7), 3);
+  })
 });
