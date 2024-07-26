8e5848b547975c8deee083db4d53bdde199e67db
wahujos <jwahu30@gmail.com>
2024-07-26 10:20:32 +0300

    added a test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,8 +17,11 @@ describe('calculateNumber', function() {
   });
   it('should round and add 1.5 and 3.7', function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
-  })
+  });
   it('should and and round a zero and a decimal number', function() {
     assert.equal(calculateNumber(0, 2.7), 3);
+  })
+  it('should round the second number correctly and give the sum', function() {
+    assert.equal(calculateNumber(3, 4.6), 8);
   })
 });
