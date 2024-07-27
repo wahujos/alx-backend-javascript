b2063f5d6cf53c2832ebef4f226b10b8618a1723
wahujos <jwahu30@gmail.com>
2024-07-27 12:44:02 +0300

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the payment system')
+    res.send('Welcome to the payment system');
 });
 app.get('/cart/:id(\\d+)', (req, res) => {
     const id = req.params.id;
-    res.send(`Payment methods for cart ${id}`)
+    res.send(`Payment methods for cart ${id}`);
 })
 app.listen(7865, () => {
     console.log('API available on localhost port 7865');
