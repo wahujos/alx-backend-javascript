230e082415a9955bedff663e9d45c35b1a9da911
wahujos <jwahu30@gmail.com>
2024-07-27 12:40:25 +0300

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Basic Integration testing', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('get / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
+    request.get(`${URL}/cart/-47`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
+    request.get(`${URL}/cart/hello`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
